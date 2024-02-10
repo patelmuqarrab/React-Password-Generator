93ce5714d82d9d6130ecd9dc8ad7d26364f1864f
patelmuqarrab <patelmuqarrab@gmail.com>
2024-02-10 19:35:51 +0530

    staters

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+// import './App.css'
+// import Card from './comp/Card';
+
+function App() {
+  const [color, setColor] = useState("red");
+  return (
+    <>
+    
+    </>
+    );
+}
+
+export default App;
